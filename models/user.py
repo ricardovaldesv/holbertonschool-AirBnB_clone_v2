1b7ef482dbe4af6a17016cb45cc331362d5ce384
ricardovaldesv <ricardovaldes@icloud.com>
2023-12-11 20:54:42 +0000

    task 7 v1

--- models/user.py
+++ models/user.py
@@ -3,8 +3,8 @@
 from models.base_model import BaseModel
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+
 
 
 class User(BaseModel, Base):
@@ -12,5 +12,5 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
