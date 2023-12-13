81e2b347777ac3580f7ccfb0740ebebae490ecf6
ricardovaldesv <ricardovaldes@icloud.com>
2023-12-13 23:16:50 +0000

    task 1 v1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,9 +12,11 @@ app = Flask(__name__)
 def hello_world():
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello():
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
