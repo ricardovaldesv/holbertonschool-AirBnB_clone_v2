9a77fce08c3277035c72bcf6ab58dc174396df6f
ricardovaldesv <ricardovaldes@icloud.com>
2023-12-14 14:59:15 +0000

    task 2 v0

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Script that starts a Flask web application """
+
+
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_Hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_is_fun(text):
+    text = text.replace('_', ' ')
+    return f'C {escape(text)}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
